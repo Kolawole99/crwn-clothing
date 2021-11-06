d44edc8f1c842c34852e6239afbe88e540e75fb1
Kolawole Ojo <philliphopeojo@gmail.com>
2021-11-06 05:27:05 +0100

    Shop Page completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>404. There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
     </div>
   );
 }
