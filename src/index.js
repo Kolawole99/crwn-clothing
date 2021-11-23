af967ce415bcb1eb36e21eba50402b76a3629958
Kolawole Ojo <philliphopeojo@gmail.com>
2021-11-23 14:35:32 +0100

    Persistence with session local storage added

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import { store, persistedStore } from './redux/store';
+
 import './index.css';
 import App from './App';
-import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistedStore}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
